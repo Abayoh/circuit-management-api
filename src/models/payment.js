a406924204d6b20242b20e13a9fb6f9af6bb13f3
“abayoh” <alexanderbayoh@hotmail.com>
2022-08-16 17:34:04 +0000

    completed mongoose payment model

---
+++ src/models/payment.js
@@ -0,0 +1,99 @@
+const mongoose = require('mongoose');
+
+const previousBalanceSchema = new mongoose.Schema(
+  {
+    paymentId: {
+      type: String,
+      trim: true,
+      required: [true, 'PaymentId is required'],
+    },
+    amount: {
+      type: Number,
+      trim: true,
+      required: [true, 'Amount is required'],
+    },
+  },
+  { _id: false, versionKey: false }
+);
+// Billed schema
+const billedSchema = new mongoose.Schema(
+  {
+    from: {
+      type: Number,
+      required: [true, 'Billed From is required'],
+    },
+    to: {
+      type: Number,
+      required: [true, 'Billed To is required'],
+    },
+  },
+  { _id: false, versionKey: false }
+);
+
+const circuitSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      required: [true, 'Circuit name is required'],
+    },
+    cost: {
+      type: Number,
+      min: 10,
+      required: [true, 'Circuit Cost is required'],
+    },
+  },
+  { _id: false, versionKey: false }
+);
+
+const PaymentSchema = new mongoose.Schema(
+  {
+    amount: {
+      type: Number,
+      trim: true,
+      required: [true, 'Amount is required'],
+    },
+    receiveBy: {
+      type: String,
+      trim: true,
+      required: [true, 'Recieveby  is required'],
+      unique: true,
+    },
+    customerName: {
+      type: String,
+      trim: true,
+      required: [true, 'Customer name is required'],
+      unique: true,
+    },
+    customerId: {
+      type: String,
+      trim: true,
+      required: [true, 'Customer Id is required'],
+      unique: true,
+    },
+    chequeId: {
+      type: String,
+      trim: true,
+      required: [true, 'ChequeId is required'],
+      unique: true,
+    },
+    circuit: [circuitSchema],
+    previousBalance: [previousBalanceSchema],
+    current: {
+      type: Boolean,
+    },
+    Billed: [billedSchema],
+    balance: {
+      type: Number,
+      min: 0,
+      required: [true, 'payment balance is required'],
+    },
+    depositedDate: {
+      type: Number,
+      required: [true, 'Deposited Date is required'],
+    },
+  },
+  { versionKey: false }
+);
+
+module.exports = mongoose.model('payment', PaymentSchema);
--- src/models/user.js
+++ src/models/user.js
@@ -14,40 +14,43 @@ const validateEmail = function (email) {
   return re.test(email);
 };
 
-const UserSchema = new mongoose.Schema({
-  fullName: {
-    type: String,
-    trim: true,
-    required: [true, 'User name is required'],
-    unique: true,
+const UserSchema = new mongoose.Schema(
+  {
+    fullName: {
+      type: String,
+      trim: true,
+      required: [true, 'User name is required'],
+      unique: true,
+    },
+    email: {
+      type: String,
+      trim: true,
+      lowercase: true,
+      unique: true,
+      required: 'Email address is required',
+      validate: [validateEmail, 'Please fill a valid email address'],
+      match: [
+        /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+        'Please fill a valid email address',
+      ],
+    },
+    role: {
+      type: String,
+      required: [true, 'Role is required'],
+      enum: ['Admin', 'Manager', 'User', 'Guest'],
+    },
+    phoneNumber: {
+      type: String,
+      required: [true, 'Phone Number is required'],
+      min: [10, 'Phone Number should be atleast ten digits'],
+      unique: true,
+    },
+    hashedPassword: {
+      type: String,
+      required: [true],
+    },
   },
-  email: {
-    type: String,
-    trim: true,
-    lowercase: true,
-    unique: true,
-    required: 'Email address is required',
-    validate: [validateEmail, 'Please fill a valid email address'],
-    match: [
-      /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
-      'Please fill a valid email address',
-    ],
-  },
-  role: {
-    type: String,
-    required: [true, 'Role is required'],
-    enum: ['Admin', 'Manager', 'User', 'Guest'],
-  },
-  phoneNumber: {
-    type: String,
-    required: [true, 'Phone Number is required'],
-    min: [10, 'Phone Number should be atleast ten digits'],
-    unique: true,
-  },
-  hashedPassword: {
-    type: String,
-    required: [true],
-  },
-});
+  { versionKey: false }
+);
 
 module.exports = mongoose.model('user', UserSchema);
